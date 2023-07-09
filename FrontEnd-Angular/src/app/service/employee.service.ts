3b24bee3bbd266c15b62299061fbf093bc87d31d
ngochung1809 <ngochung1809@kali.kali>
2023-07-09 23:52:56 +0700

    upgrade port server

--- FrontEnd-Angular/src/app/service/employee.service.ts
+++ FrontEnd-Angular/src/app/service/employee.service.ts
@@ -8,7 +8,7 @@ import { Employee } from '../model/employee';
 })
 export class EmployeeService {
 
-  private baseURL = "http://localhost:8888/api/v1/employees";
+  private baseURL = "http://localhost:8080/api/v1/employees";
   
   constructor(private httpClient:HttpClient) { }
 
